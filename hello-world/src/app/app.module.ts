d3a601649f23a786b39f9223488c5ee6fd72acd0
desiwork0 <127345400+desiwork0@users.noreply.github.com>
2023-06-14 19:03:22 +0000

    Promjena u codespaces.

--- hello-world/src/app/app.module.ts
+++ hello-world/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TextckmponentComponent } from './textckmponent/textckmponent.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TextckmponentComponent
   ],
   imports: [
     BrowserModule,
---
+++ hello-world/src/app/textckmponent/textckmponent.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-textckmponent',
+  templateUrl: './textckmponent.component.html',
+  styleUrls: ['./textckmponent.component.css']
+})
+export class TextckmponentComponent {
+
+}
