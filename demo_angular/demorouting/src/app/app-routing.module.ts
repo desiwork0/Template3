36519a109c5b15da1565094a2edae144a64bceea
desiwork0 <desiwork0@gmail.com>
2023-06-25 15:50:40 +0200

    dodan routing

--- demo_angular/demorouting/src/app/app-routing.module.ts
+++ demo_angular/demorouting/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HrvComponent } from './hrv/hrv.component';
+import { EngComponent } from './eng/eng.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'eng', component: EngComponent},
+  {path: 'hrv', component: HrvComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
